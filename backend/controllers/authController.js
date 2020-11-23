b86f6deccac6bfbd2a7a0c4a5857f2932ce5cd00
cnikols96 <costanikols8@gmail.com>
2020-11-22 23:52:02 -0600

    Co-authored-by: Aditya, Tejas <taditya2@illinois.edu>

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,58 @@
-module.exports = {
-    printSomething: async(req, res)=>{ // request and result 
-        console.log('I printed!');
-        //fetching some data
 
-        res.status(200).send(data);
-        // res.sendStatus(200);
+/* Example on how authController replys and how auth should talk to the server
+* When running it will respond with a 200 okay and send the print statement back over to print on main page
+*
+*/
+// module.exports = {
+//     printSomething: async(req, res)=>{ // request and result 
+//         console.log('I printed!');
+//         res.status(200).send("hello world");
+//         //fetching some data
+
+//         // res.status(200).send();
+//         // res.sendStatus(200);
+//     }
+// };
+
+/*express-session: Creates a session that allows the user to login and do all the fun things. 
+*does not inhearently track the user need to assign identifiers uninquly for each new user if you want to track
+*/
+const express = require('express')
+const express = require('express-session')
+
+const app = express()
+app.use(session({
+    'secret': 'authPassword212'
+}))
+
+// app.get('/', (req, res, next) => {
+//     // req.session 
+// }
+
+
+
+app.get('/', function(req, res, next) {
+    if (req.session.views) {
+      req.session.views++
+      res.setHeader('Content-Type', 'text/html')
+      res.write('<p>views: ' + req.session.views + '</p>')
+      res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+      res.end()
+    } else {
+      req.session.views = 1
+      res.end('welcome to the session demo. refresh!')
     }
-};
+  })
 
-//express-session
-//login
-//getUser
-//logout
+//login: 
+
+
+
+
+//getUser:
+
+
+
+
+//logout: 
+
--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,22 @@ require('dotenv').config();
 
 const express = require('express');
 
+// Costa added
+const session = require('express-session');
+
 const {SERVER_PORT} = process.env;
 
 // // // // CONTROLLERS // // // //
 
 const app = express();
-const ac = require('./controllers/authController');
 
+//Costa Added
+//unique string as a password parameter 
+app.use(session ({
+    'secret': 'password'
+}))
+
+const ac = require('./controllers/authController');
 // // // // ENDPOINTS // // // //
 
 
@@ -18,9 +27,18 @@ app.listen(SERVER_PORT,()=>{
     console.log(`Magic at ${SERVER_PORT}`);
 });
 
-app.get(`/info/print`, ac.printSomething);
+/* Example code for how to talk to authController and how to get responses 
+* Does nothing besides printing the info from the printSomething in authController
+* Costa Added
+*/
+// app.get(`/`, ac.printSomething);
+// console.log("main page"); 
+// app.get(`/info/print`, ac.printSomething);
+// console.log("got the info from /info/print");
 
-// app.get => get info
+
+
+//app.get => get info
 // app.post => create info
 // app.delete => delete info
 // app.put => update info
