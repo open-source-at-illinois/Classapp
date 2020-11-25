2c20fbcd727ed1871b0f5bb4599259ed7a0cde9e
tuedo2 <tuedo2@illinois.edu>
2020-11-25 13:37:36 -0600

    I don't know what I'm doing

--- backend/controllers/infoController.js
+++ backend/controllers/infoController.js
@@ -1,19 +1,35 @@
-module.exports = {
-    printAnything: async(req, res)=>{ // request and result 
-        console.log('I have printed anything!');
-        //fetching some data
+// module.exports = {
+//     printAnything: async(req, res)=>{ // request and result 
+//         console.log('I have printed anything!');
+//         //fetching some data
 
-        data = {a:1,b:2,c:3,};
-        res.status(200).send(data);
+//         data = {a:1,b:2,c:3,};
+//         res.status(200).send(data);
         
-        // res.sendStatus(200);
-    },
+//         // res.sendStatus(200);
+//     },
 
-    postAnything: async(req, res)=>{
-        console.log(req.body);
-        res.sendStatus(200);
+//     postAnything: async(req, res)=>{
+//         console.log(req.body);
+//         res.sendStatus(200);
+//     }
+// };
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://compass2g.illinois.edu', true);
+
+request.onload = function(){
+    var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 400) {
+        console.log('success!');
+    } else {
+        console.log('error');
     }
-};
+}
+
+request.send();
 
 //express-session
 //login
